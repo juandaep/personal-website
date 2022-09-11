d0b130a7226a20eb7e5210f8be1ebdf68d02d0a5
Juanda Esa Putra <juandaesaputra@gmail.com>
2022-09-11 21:15:00 +0700

    Configure React app for deployment to GitHub Pages

---
+++ src/components/Undermaintenance.js
@@ -0,0 +1,27 @@
+import React from "react";
+import banner from "../img/under-maintenance.svg";
+
+const Undermaintenance = () => {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center px-10">
+      <img src={banner} alt="Undermaintenance" className="w-[31rem] mb-10" />
+      <div className="md:hidden flex flex-col items-center">
+        <h1 className="text-3xl font-semibold">Website Under</h1>
+        <h1 className="text-3xl font-semibold">
+          {"< "}
+          <span className="bg-[#4C4DDC] text-white mb-2">Development</span>{" "}
+          {"/>"}
+        </h1>
+      </div>
+      <div className="invisible md:visible flex flex-col items-center">
+        <h1 className="text-3xl font-semibold">Website Under</h1>
+        <h1 className="text-3xl font-semibold">
+          {"< "}
+          <span className="bg-primary text-white mb-2">Development</span> {"/>"}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Undermaintenance;
